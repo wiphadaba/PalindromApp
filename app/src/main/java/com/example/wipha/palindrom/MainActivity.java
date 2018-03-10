947b8b477d5cd223e974b56e2b6a012273ba1af4
Wiphada Baumgartner <wiphadaba@edu.aau.at>
2018-03-11 00:40:07 +0100

    Grundfunktion implementiert: Prüft mit Ruecksicht auf Großbuchstaben, ob das eingebene Wort ein Palindrom ist. In MainActivity: resultTextView naeher an dem Button geschoben

--- app/src/main/java/com/example/wipha/palindrom/MainActivity.java
+++ app/src/main/java/com/example/wipha/palindrom/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.wipha.palindrom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button checkButton = findViewById(R.id.checkButton);
+        checkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText inputEditText = findViewById(R.id.inputEditText);
+                TextView resultTextView = findViewById(R.id.resultTextView);
+
+               String input = inputEditText.getText().toString();
+
+                if(isPalindrom(input)){
+                    resultTextView.setText("Das Wort "+input+" ist ein Palindrom!");
+                }else{
+                    resultTextView.setText("Das Wort "+input+" ist kein Palindrom!");
+                }
+
+
+            }
+        });
+    }
+
+
+    // check if the given string is a palindrom ignoring lower and upper case respectively
+    public static boolean isPalindrom(String str){
+        return str.equalsIgnoreCase(new StringBuilder(str).reverse().toString());
     }
 }
